c6d011bdcda1f5cc2b408ed0fe07dcd66093b292
Anriu Oliveira <anriu.oliveira12@gmail.com>
2026-06-15 16:50:13 -0300

    Criação do segundo banco de dados, assim como a insersão de valores nas tabelas

---
+++ Banco 1 - Questões 26 a 50.sql
@@ -0,0 +1,38 @@
+-- Banco 2
+--Tabela
+
+
+
+
+
+-- Questão 26
+-- Questão 27
+-- Questão 28
+-- Questão 29
+-- Questão 30
+-- Questão 31
+-- Questão 32
+-- Questão 33
+-- Questão 34
+-- Questão 35
+-- Questão 36
+-- Questão 37
+-- Questão 38
+-- Questão 39
+-- Questão 40
+-- Questão 41
+-- Questão 42
+-- Questão 43
+-- Questão 44
+-- Questão 45
+-- Questão 46
+-- Questão 47
+-- Questão 48
+-- Questão 49
+-- Questão 50
+
+
+
+
+
+-- Inserções para teste
